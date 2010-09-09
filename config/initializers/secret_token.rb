57cd0dd8c5c860d97d94f88c91ddc997c8ed2a11
ryebread <hhdslb@gmail.com>
2010-09-09 09:58:40 +0800

    init efm project with rails 3.0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Efm::Application.config.secret_token = 'ac551fa58a164f4dd91b9df69ea9ec81449ad19f2ee0155e22b65e4ec5c75f180174e0e3bd98b631e04ec3627e02dc795836829340ea102c468f1492e80288b7'
